ab8ba279bb3fbdf4f01039c3f31f4fdbc11995a8
David <115666355+David-237-9@users.noreply.github.com>
2025-10-03 21:30:59 +0100

    Storage Test Improvement
    Now it saves all stored files

--- VideoServer/src/test/kotlin/storage/StorageTests.kt
+++ VideoServer/src/test/kotlin/storage/StorageTests.kt
@@ -4,12 +4,15 @@ import java.io.File
 import kotlin.test.Test
 
 private const val STORAGE_PATH = "storage"
+private const val TEMP_STORAGE_PATH = "temp_storage"
 
 class StorageTests {
     @Test
     fun initTest() {
         // Ensure a clean state before test
         val storageDir = File(STORAGE_PATH)
+        val tempStorageDir = File(TEMP_STORAGE_PATH)
+        moveAllFilesAndFolders(storageDir, tempStorageDir)
         storageDir.deleteRecursively()
 
         // Execute
@@ -20,5 +23,15 @@ class StorageTests {
 
         // Cleanup after test
         storageDir.deleteRecursively()
+        moveAllFilesAndFolders(tempStorageDir, storageDir)
+        tempStorageDir.deleteRecursively()
+    }
+
+    fun moveAllFilesAndFolders(sourceDir: File, targetDir: File) {
+        if (!targetDir.exists()) targetDir.mkdirs()
+        sourceDir.listFiles()?.forEach { file ->
+            val target = File(targetDir, file.name)
+            file.renameTo(target)
+        }
     }
 }
