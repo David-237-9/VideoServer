2e19f40ad356ac410dd851daa04b0c5630b7a2db
David <115666355+David-237-9@users.noreply.github.com>
2025-10-04 14:51:43 +0100

    Server Logger Fixing

--- VideoServer/build.gradle.kts
+++ VideoServer/build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 dependencies {
     implementation("io.ktor:ktor-server-core:2.3.1")
     implementation("io.ktor:ktor-server-netty:2.3.1")
+    implementation("org.slf4j:slf4j-simple:2.0.13")
 
     testImplementation(kotlin("test"))
 }
